e23f93a0a8147f6501fc2a47ffcb64c9c4d8db67
Ethan Tribe <ethantribee@gmail.com>
2026-01-24 16:34:26 +0000

    Additional L-Systems

--- Lindenmayer System.py
+++ Lindenmayer System.py
@@ -3,11 +3,11 @@ import turtle
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 debugMode = False
-colours = []
+debugColours = []
 for r in [0, 0.5, 1]:
     for b in [0, 0.5, 1]:
         for g in [0, 0.5, 1]:
-            colours.append((r,b,g))
+            debugColours.append((r,b,g))
 
 def GetMap(rules, char):
     if char in rules:
@@ -34,8 +34,8 @@ def CalculateStep(turtle, tree, dist, angle):
             turtle.pendown()
         elif char in alphabet:
             if (debugMode):
-                turtle.color(colours[alphabet.index(char)])
-                turtle.write(f"   {char}", font=("Arial", 10, "bold"))
+                turtle.color(debugColours[alphabet.index(char)])
+                #turtle.write(f"   {char}", font=("Arial", 10, "bold"))
             turtle.forward(dist)
         elif char == "+":
             turtle.left(angle)
@@ -47,25 +47,73 @@ def CalculateStep(turtle, tree, dist, angle):
 
 
 # The system's variables
-name = "Barnsley Fern"
-rules = {
-    "X": "F+[[X]-X]-F[-FX]+X",
-    "F": "FF"
-}
-axiom = "-X"
-angle = 25
-colour = "olive drab"
-startPos = ((-100, -450))
+systems = [
+    {
+        "name": "Barnsley Fern",
+        "rules": {
+            "X": "F+[[X]-X]-F[-FX]+X",
+            "F": "FF"
+        },
+        "axiom": "-X",
+        "angle": 25,
+        "colour": "olive drab",
+        "startPos": ((-100, -450))
+    },
+    {
+        "name": "Sierpinski arrowhead curve",
+        "rules": {
+            "A": "B-A-B",
+            "B": "A+B+A"
+        },
+        "axiom": "A",
+        "angle": 60,
+        "colour": "hot pink",
+        "startPos": ((0, -400))
+    },
+    {
+        "name": "Christmas Tree",
+        "rules": {
+            "A": "BBB[++[+D][C][-D]][BA][--[+D][C][-D]]",
+            "C": "BB[+D][C][-D]",
+            "D": "E[+E][E][-E]"
+        },
+        "axiom": "A",
+        "angle": 45,
+        "colour": "olive drab",
+        "startPos": ((0, -450))
+    }
+]
 
+# User input to choose system
+for i in range(len(systems)):
+    print(str(i + 1) + ". " + systems[i]["name"])
+
+reinput = True
+while (reinput):
+    reinput = False
+    choice = input("\nChoose a Lindenmayer system from the above list: ")
+
+    try:
+        choice = int(choice)
+    except ValueError:
+        print("Invalid input - try again")
+        reinput = True
+
+    if (not reinput):
+        choice -= 1
+        if (choice not in range(len(systems))):
+            print("Number not in range - try again")
+            reinput = True
+
+system = systems[choice]
 
 # Generate the L-System
-
 print()
 
-tree = axiom
+tree = system["axiom"]
 
 screen = turtle.Screen()
-screen.title(name)
+screen.title(system["name"])
 screen.setup(1000, 1000)
 screen.bgcolor("azure")
 screen.tracer(0, 0)
@@ -74,21 +122,21 @@ turtle = turtle.Turtle()
 turtle.pensize(2)
 turtle.speed("fastest")
 turtle.hideturtle()
-turtle.color(colour)
+turtle.color(system["colour"])
 
 def Step():
     global tree
 
     turtle.clear()
     turtle.penup()
-    turtle.goto(startPos)
+    turtle.goto(system["startPos"])
     turtle.setheading(90)
     turtle.pendown()
 
-    CalculateStep(turtle, tree, 10, angle)
+    CalculateStep(turtle, tree, 20, system["angle"])
     screen.update()
 
-    tree = GetTree(rules, tree)
+    tree = GetTree(system["rules"], tree)
 
 def ToggleDebugMode():
     global debugMode
@@ -96,11 +144,11 @@ def ToggleDebugMode():
     if (debugMode):
         print("Debug colouring on")
     else:
-        turtle.color(colour)
+        turtle.color(system["colour"])
         print("Debug colouring off")
 
 def SaveImage():
-    screen.getcanvas().postscript(file=f"{name}.ps")
+    screen.getcanvas().postscript(file=system["name"] + ".ps")
     print("Saved image")
 
 Step()
